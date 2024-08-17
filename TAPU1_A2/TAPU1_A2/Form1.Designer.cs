52444e52dfb53801abb22f8636aef5103c0930bf
JEQS <104605294+JaimeQuistian@users.noreply.github.com>
2024-08-17 16:44:54 -0600

    FInalVersion_RunS

---
+++ TAPU1_A2/TAPU1_A2/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace TAPU1_A2
+{
+    partial class frmRunS
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmRunS));
+            this.anmPersona = new TAPU2_LibreriaControles.Animacion();
+            this.tmrJoyStick = new System.Windows.Forms.Timer(this.components);
+            this.tmrMovimiento = new System.Windows.Forms.Timer(this.components);
+            this.lblPlayer1 = new System.Windows.Forms.Label();
+            this.lblPlayer2 = new System.Windows.Forms.Label();
+            this.anmPersona2 = new TAPU2_LibreriaControles.Animacion();
+            this.tmrMovimientoP2 = new System.Windows.Forms.Timer(this.components);
+            this.cbListasJoysticks = new System.Windows.Forms.ComboBox();
+            this.cbListas2 = new System.Windows.Forms.ComboBox();
+            this.tblTexP2 = new System.Windows.Forms.Label();
+            this.lblTextP1 = new System.Windows.Forms.Label();
+            this.tmrJoystick2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // anmPersona
+            // 
+            this.anmPersona.BackColor = System.Drawing.Color.Transparent;
+            this.anmPersona.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("anmPersona.BackgroundImage")));
+            this.anmPersona.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.anmPersona.Location = new System.Drawing.Point(1, 331);
+            this.anmPersona.Name = "anmPersona";
+            this.anmPersona.Size = new System.Drawing.Size(94, 102);
+            this.anmPersona.TabIndex = 0;
+            // 
+            // tmrJoyStick
+            // 
+            this.tmrJoyStick.Tick += new System.EventHandler(this.tmrJoyStick_Tick);
+            // 
+            // tmrMovimiento
+            // 
+            this.tmrMovimiento.Tick += new System.EventHandler(this.tmrMovimiento_Tick);
+            // 
+            // lblPlayer1
+            // 
+            this.lblPlayer1.AutoSize = true;
+            this.lblPlayer1.BackColor = System.Drawing.Color.Transparent;
+            this.lblPlayer1.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer1.Location = new System.Drawing.Point(29, 436);
+            this.lblPlayer1.Name = "lblPlayer1";
+            this.lblPlayer1.Size = new System.Drawing.Size(64, 17);
+            this.lblPlayer1.TabIndex = 2;
+            this.lblPlayer1.Text = "Player 1";
+            // 
+            // lblPlayer2
+            // 
+            this.lblPlayer2.AutoSize = true;
+            this.lblPlayer2.BackColor = System.Drawing.Color.Transparent;
+            this.lblPlayer2.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer2.Location = new System.Drawing.Point(29, 316);
+            this.lblPlayer2.Name = "lblPlayer2";
+            this.lblPlayer2.Size = new System.Drawing.Size(68, 17);
+            this.lblPlayer2.TabIndex = 3;
+            this.lblPlayer2.Text = "Player 2";
+            // 
+            // anmPersona2
+            // 
+            this.anmPersona2.BackColor = System.Drawing.Color.Transparent;
+            this.anmPersona2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("anmPersona2.BackgroundImage")));
+            this.anmPersona2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.anmPersona2.Location = new System.Drawing.Point(1, 211);
+            this.anmPersona2.Name = "anmPersona2";
+            this.anmPersona2.Size = new System.Drawing.Size(94, 102);
+            this.anmPersona2.TabIndex = 4;
+            // 
+            // tmrMovimientoP2
+            // 
+            this.tmrMovimientoP2.Tick += new System.EventHandler(this.tmrMovimientoP2_Tick);
+            // 
+            // cbListasJoysticks
+            // 
+            this.cbListasJoysticks.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cbListasJoysticks.FormattingEnabled = true;
+            this.cbListasJoysticks.Location = new System.Drawing.Point(12, 12);
+            this.cbListasJoysticks.Name = "cbListasJoysticks";
+            this.cbListasJoysticks.Size = new System.Drawing.Size(245, 21);
+            this.cbListasJoysticks.TabIndex = 1;
+            this.cbListasJoysticks.Text = "Conectar Joystick";
+            // 
+            // cbListas2
+            // 
+            this.cbListas2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cbListas2.FormattingEnabled = true;
+            this.cbListas2.Location = new System.Drawing.Point(543, 12);
+            this.cbListas2.Name = "cbListas2";
+            this.cbListas2.Size = new System.Drawing.Size(245, 21);
+            this.cbListas2.TabIndex = 6;
+            this.cbListas2.Text = "Conectar Joystick";
+            this.cbListas2.SelectionChangeCommitted += new System.EventHandler(this.cbListas2_SelectionChangeCommitted);
+            // 
+            // tblTexP2
+            // 
+            this.tblTexP2.AutoSize = true;
+            this.tblTexP2.BackColor = System.Drawing.Color.Transparent;
+            this.tblTexP2.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tblTexP2.ForeColor = System.Drawing.Color.Red;
+            this.tblTexP2.Location = new System.Drawing.Point(546, 36);
+            this.tblTexP2.Name = "tblTexP2";
+            this.tblTexP2.Size = new System.Drawing.Size(258, 17);
+            this.tblTexP2.TabIndex = 7;
+            this.tblTexP2.Text = "Player 2: Seleccionar para conectar";
+            // 
+            // lblTextP1
+            // 
+            this.lblTextP1.AutoSize = true;
+            this.lblTextP1.BackColor = System.Drawing.Color.Transparent;
+            this.lblTextP1.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTextP1.ForeColor = System.Drawing.Color.Blue;
+            this.lblTextP1.Location = new System.Drawing.Point(9, 36);
+            this.lblTextP1.Name = "lblTextP1";
+            this.lblTextP1.Size = new System.Drawing.Size(203, 17);
+            this.lblTextP1.TabIndex = 8;
+            this.lblTextP1.Text = "Player 1: Clic para conectar";
+            // 
+            // tmrJoystick2
+            // 
+            this.tmrJoystick2.Tick += new System.EventHandler(this.tmrJoystick2_Tick);
+            // 
+            // frmRunS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::TAPU1_A2.Properties.Resources.Paisaje;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 461);
+            this.Controls.Add(this.lblTextP1);
+            this.Controls.Add(this.tblTexP2);
+            this.Controls.Add(this.cbListas2);
+            this.Controls.Add(this.anmPersona2);
+            this.Controls.Add(this.lblPlayer2);
+            this.Controls.Add(this.lblPlayer1);
+            this.Controls.Add(this.cbListasJoysticks);
+            this.Controls.Add(this.anmPersona);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(816, 500);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(816, 500);
+            this.Name = "frmRunS";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RunS";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.frmRunS_Load);
+            this.Click += new System.EventHandler(this.frmRunS_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TAPU2_LibreriaControles.Animacion anmPersona;
+        private System.Windows.Forms.Timer tmrJoyStick;
+        private System.Windows.Forms.Timer tmrMovimiento;
+        private System.Windows.Forms.Label lblPlayer1;
+        private System.Windows.Forms.Label lblPlayer2;
+        private TAPU2_LibreriaControles.Animacion anmPersona2;
+        private System.Windows.Forms.Timer tmrMovimientoP2;
+        private System.Windows.Forms.ComboBox cbListasJoysticks;
+        private System.Windows.Forms.ComboBox cbListas2;
+        private System.Windows.Forms.Label tblTexP2;
+        private System.Windows.Forms.Label lblTextP1;
+        private System.Windows.Forms.Timer tmrJoystick2;
+    }
+}
+
---
+++ TAPU1_A2/TAPU1_A2/Form1.cs
@@ -0,0 +1,281 @@
+﻿/*
+Nombre de la practica: RunS
+Tema de Referencia: Componentes y Librerias
+Objetivo: Desarrollar una aplicacón que permita inplementar componentes definidos por el usuario, y componentes
+          integrados de .Net
+Nombres: Adrián Ortíz Quistián
+         Jaime Emnuel Quitián sánchez
+Materia: Topicos Avanzados de Programacón
+*/
+
+//---------------- Librerias ----------------
+//Seleción de librerias del sistema
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//Agregar las librerias para configuarar el joystick
+using AForge;
+using AForge.Controls;
+
+//---------------- Código ----------------
+namespace TAPU1_A2
+{
+    public partial class frmRunS : Form
+    {
+        //---------------- Variables globales ----------------
+        //Creación de las variables globales
+        //Inicio declaracion de clases para los Joysticks declarados en un inicio como variables
+        Joystick joystick;
+        Joystick joystick2;
+        //Creación de una lista global para utilizarla en el seleccionado de los controles
+        List<Joystick.DeviceInfo> dispositivosJoysticks = Joystick.GetAvailableDevices();
+        //Creación la clase de tipo Random para asignarles velocidades aleatorias a los Personajes
+        Random velocidad = new Random();
+        DialogResult victoria = new DialogResult();
+        System.Drawing.Point ubicacion= new System.Drawing.Point(1,331);
+        System.Drawing.Point ubicacion2 = new System.Drawing.Point(1, 211);
+        //---------------- Constructor ----------------
+        public frmRunS()
+        {
+            InitializeComponent();
+            //Decalaración de la propiedad DoubleBuffered para evitar el parpadeo del personaje durante su activación
+            this.DoubleBuffered = true;
+
+        }
+
+        //---------------- Eventos ----------------
+
+        //---------------- Evento Click ----------------
+        //Declaración del evento click para asignar un Joystick al primer jugador
+        private void frmRunS_Click(object sender, EventArgs e)
+        {
+            
+            //Instansiación de un dialog result para preguntarle al usuario si ese es el control que desea asignar
+            //para el primer jugador y conectarlo
+            DialogResult conexion = new DialogResult();
+            //Declaración del try y catch para atrapar errores en caso de presentarse 
+            try
+            {
+                //Termino de la creación de la clase para el Joystick con el paramétro del combobox que guardá la ID
+                //del joystick a conectar
+                joystick = new Joystick(cbListasJoysticks.SelectedIndex);
+                //Información que mostrará el Dialog Result como su texto, título, opciones e icono  
+                conexion = MessageBox.Show("¿Quieres conectarte?", "Conexion", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                //Comparación de la respuesta dada por el usuario para activar o desactivar el timer del jugador 1
+                if (conexion == DialogResult.Yes)
+                {
+                    //Si la respuesta es si entonces se activará el timer para la conexión del Joystick con el
+                    //personaje
+                    tmrJoyStick.Enabled = true;
+                }
+                else
+                {
+                    //Si la respuesta es no entonces no se activará el timer para la conexión del Joystick con el
+                    //personaje
+                    tmrJoyStick.Enabled = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                //En caso de no detectar un Joystick para la conexión  el catch mostrará un mensaje de error  
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        //---------------- Evento SelectChangeComited ----------------
+
+        //Declaración del evento SelectionChangeCommitted para asignar un Joystick al segundo jugador
+        private void cbListas2_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            //Instansiación de un dialog result para preguntarle al usuario si ese es el control que desea asignar
+            //para el segundo jugador y conectarlo
+            DialogResult conexion = new DialogResult();
+            //Declaración del try y catch para atrapar errores en caso de presentarse
+            try
+            {
+
+                //Termino de la creación de la clase para el Joystick con el paramétro del combobox que guardá la ID
+                //del joystick a conectar
+                joystick2 = new Joystick(cbListas2.SelectedIndex);
+                //Información que mostrará el Dialog Result como su texto, título, opciones e icono
+                conexion = MessageBox.Show("¿Quieres conectarte?", "Conexion", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                //Comparación de la respuesta dada por el usuario para activar o desactivar el timer del jugador 2
+                if (conexion == DialogResult.Yes)
+                {
+
+                    //Si la respuesta es si entonces se activará el timer para la conexión del Joystick con el
+                    //personaje
+                    tmrJoystick2.Enabled = true;
+                }
+                else
+                {
+                    //Si la respuesta es no entonces no se activará el timer para la conexión del Joystick con el
+                    //personaje
+                    tmrJoystick2.Enabled = false;
+                }
+            }
+            //En caso de no detectar un Joystick para la conexión  el catch mostrará un mensaje de error 
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        //---------------- Evento Load ----------------
+
+        //Declaración del evento Load para cargar la lista de los controles que se mostrarán en los combobox 
+        private void frmRunS_Load(object sender, EventArgs e)
+        {
+            
+            //Creación de Foreach con los controles disponibles para el jugador 1
+            foreach (Joystick.DeviceInfo joystick in dispositivosJoysticks)
+            {
+                cbListasJoysticks.Items.Add(string.Format("ID: {0}, Nombre: {1} ...", joystick.ID+1, joystick.Name));
+            }
+            //Creación de Foreach con los controles disponibles para el jugador 2
+            foreach (Joystick.DeviceInfo joystick in dispositivosJoysticks)
+            {
+                cbListas2.Items.Add(string.Format("ID: {0}, Nombre: {1} ...", joystick.ID + 1, joystick.Name));
+            }
+        }
+
+        //---------------- Joystick 1 ----------------
+
+        //---------------- Evento Tick Para Joystik1 ----------------
+
+        //Declaración del primer timer para el jugador 1 con las acciones de su control
+        private void tmrJoyStick_Tick(object sender, EventArgs e)
+        {
+            //Declaración del try y catch para atrapar errores en caso de presentarse
+            try
+            {
+                //Creación de una clase para guardar el estatus del joystick para guardarlo dentro de la
+                //variable joystick
+                Joystick.Status status = joystick.GetCurrentStatus();
+                //Si se preciona el botón 2 se desactivará el timer que controla el movimiento del personaje  
+                if (status.XAxis==1)
+                {
+                    //Declarar la variable para asignarle valores randoms para la velocidad que tendrá el personaje
+                    int personaVelocidad = velocidad.Next(1, 10);
+                    //Aumento de velocidad del personaje con los valores randoms declarados 
+                    anmPersona.Left = anmPersona.Left + personaVelocidad;
+                }
+                //Si se preciona el botón 1 se activará el timer que controla el movimiento del personaje  
+                if (status.XAxis == -1)
+                {
+                    //Declarar la variable para asignarle valores randoms para la velocidad que tendrá el personaje
+                    int personaVelocidad = velocidad.Next(1, 10);
+                    //Aumento de velocidad del personaje con los valores randoms declarados 
+                    anmPersona.Left = anmPersona.Left - personaVelocidad;
+                }
+                //Si se preciona el botón 3 se activará un salto que después de un tiempo bajará simulando un salto
+                //del personaje  
+                if (status.YAxis==-1)
+                {
+                    tmrMovimiento.Enabled = true;
+                }
+                //Se utiliza la propiedad right para obtener la distancia en pixeles del borde derecho al izquierdo 
+                //que se comparará con el tamaño de la ventana por medio de la propiedad ClientSize.Width 
+                //para conocer su tamaño
+                if (anmPersona.Right >= ClientSize.Width)
+                {
+                    anmPersona.Location = ubicacion;
+                    anmPersona2.Location = ubicacion2;
+                    //Se desactiva el timer del movimiento para activar el movimiento en reversa del personaje
+                    victoria =MessageBox.Show("El ganador es el Jugador 1","Ganador",MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                if (anmPersona.Right < 100)
+                {
+                    anmPersona.Location = ubicacion;
+                }
+            }
+            //En caso de no detectar un Joystick para la conexión  el catch mostrará un mensaje de error
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+
+        }
+
+        //---------------- Evento Tick del Joystik 1 ----------------
+
+        //Decalaración del evento para controlar el movimiento hacia adelante del personaje
+        private void tmrMovimiento_Tick(object sender, EventArgs e)
+        {
+            anmPersona.Top -= 20;
+            Thread.Sleep(80);
+            anmPersona.Top += 20;
+            tmrMovimiento.Stop();
+        }
+
+        //---------------- Joystick 2 ----------------
+
+        //---------------- Evento Tick Para Joystik2 ----------------
+
+        //Declaración del primer timer para el jugador 2 con las acciones de su control
+        private void tmrJoystick2_Tick(object sender, EventArgs e)
+        {
+            //Declaración del try y catch para atrapar errores en caso de presentarse
+            try
+            {
+                //Creación de una clase para guardar el estatus del joystick para guardarlo dentro de la
+                //variable joystick
+                Joystick.Status status = joystick2.GetCurrentStatus();
+                //Si se preciona el botón 2 se desactivará el timer que controla el movimiento del personaje  
+                if (status.XAxis == 1)
+                {
+                    //Declarar la variable para asignarle valores randoms para la velocidad que tendrá el personaje
+                    int personaVelocidad = velocidad.Next(1, 10);
+                    //Aumento de velocidad del personaje con los valores randoms declarados 
+                    anmPersona2.Left = anmPersona2.Left + personaVelocidad;
+                }
+                //Si se preciona el botón 1 se activará el timer que controla el movimiento del personaje  
+                if (status.XAxis == -1)
+                {
+                    //Declarar la variable para asignarle valores randoms para la velocidad que tendrá el personaje
+                    int personaVelocidad = velocidad.Next(1, 10);
+                    //Aumento de velocidad del personaje con los valores randoms declarados 
+                    anmPersona2.Left = anmPersona2.Left - personaVelocidad;
+                }
+                //Si se preciona el botón 3 se activará un salto que después de un tiempo bajará simulando un salto
+                //del personaje  
+                if (status.YAxis == -1)
+                {
+                    tmrMovimiento.Enabled = true;
+                }
+                //Se utiliza la propiedad right para obtener la distancia en pixeles del borde derecho al izquierdo 
+                //que se comparará con el tamaño de la ventana por medio de la propiedad ClientSize.Width 
+                //para conocer su tamaño
+                if (anmPersona.Right >= ClientSize.Width)
+                {
+                    anmPersona2.Location = ubicacion2;
+                    anmPersona.Location = ubicacion;
+                    //Se desactiva el timer del movimiento para activar el movimiento en reversa del personaje
+                    victoria = MessageBox.Show("El ganador es el Jugador 2", "Ganador", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    
+                }
+                if (anmPersona2.Right < 100)
+                {
+                    anmPersona2.Location = ubicacion2;
+                }
+            }
+            //En caso de no detectar un Joystick para la conexión  el catch mostrará un mensaje de error
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+        //---------------- Evento Tick del Joystik 2 ----------------
+        //Decalaración del evento para controlar el movimiento hacia adelante del personaje
+        private void tmrMovimientoP2_Tick(object sender, EventArgs e)
+        {
+            anmPersona2.Top -= 20;
+            Thread.Sleep(80);
+            anmPersona2.Top += 20;
+            tmrMovimientoP2.Stop();
+        }
+    }
+}
